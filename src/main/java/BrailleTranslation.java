ba8d88ce273d0ccfebc3feefa6b038ca8b6bbfa7
Rahul Yarlagadda <knufire@gmail.com>
2020-05-25 22:30:33 -0400

    Completed braille-translation problem.

--- src/main/java/BrailleTranslation.java
+++ src/main/java/BrailleTranslation.java
@@ -1,9 +1,40 @@
 public class BrailleTranslation {
 
     public static class Solution {
+
+        public static final String[] BRAILLE_CODES = { "100000", "110000", "100100", "100110", "100010", "110100",
+                "110110", "110010", "010100", "010110", "101000", "111000", "101100", "101110", "101010", "111100",
+                "111110", "111010", "011100", "011110", "101001", "111001", "010111", "101101", "101111", "101011" };
+
+        public static final String BRAILLE_SPACE = "000000";
+        public static final String BRAILLE_CAPITALIZATION = "000001";
+
         public static String solution(String s) {
-            return ""; //TODO
+            StringBuilder output = new StringBuilder();
+            if (s != null) {
+                for (char c : s.toCharArray()) {
+                    output.append(getBrailleCode(c));
+                }
+            }
+            return output.toString();
+        }
+
+        public static String getBrailleCode(char c) {
+            if (Character.valueOf(' ').equals(c)) {
+                return BRAILLE_SPACE;
+            }
+
+            String output = "";
+            if (Character.isUpperCase(c)) {
+                output += BRAILLE_CAPITALIZATION;
+                c = Character.toLowerCase(c);
+            }
+            output += BRAILLE_CODES[getIndexFromLowercaseAsciiCode(c)];
+            return output;
+        }
+
+        public static int getIndexFromLowercaseAsciiCode(char c) {
+            return (int) c - 97;
         }
     }
-    
 }
--- src/test/java/BrailleTranslationTests.java
+++ src/test/java/BrailleTranslationTests.java
@@ -4,6 +4,18 @@ import org.junit.Test;
 public class BrailleTranslationTests {
 
     @Test
+    public void testEmptyString() {
+        String s = BrailleTranslation.Solution.solution("");
+        assertEquals(s, "");
+    }
+
+    @Test
+    public void testNull() {
+        String s = BrailleTranslation.Solution.solution(null);
+        assertEquals(s, "");
+    }
+
+    @Test
     public void testWord() {
         String s = BrailleTranslation.Solution.solution("code");
         assertEquals(s, "100100101010100110100010");
@@ -18,7 +30,8 @@ public class BrailleTranslationTests {
     @Test
     public void testSentence() {
         String s = BrailleTranslation.Solution.solution("The quick brown fox jumps over the lazy dog");
-        assertEquals(s, "000001011110110010100010000000111110101001010100100100101000000000110000111010101010010111101110000000110100101010101101000000010110101001101100111100011100000000101010111001100010111010000000011110110010100010000000111000100000101011101111000000100110101010110110");
+        assertEquals(s,
+                "000001011110110010100010000000111110101001010100100100101000000000110000111010101010010111101110000000110100101010101101000000010110101001101100111100011100000000101010111001100010111010000000011110110010100010000000111000100000101011101111000000100110101010110110");
     }
-    
+
 }
