9f9f80101af2d8d02a2d1b6d0988a6d63ff3c2a9
Rahul Yarlagadda <knufire@gmail.com>
2020-05-25 21:57:47 -0400

    Add provided materials and tests for braille-translation problem.

---
+++ src/main/java/BrailleTranslation.java
@@ -0,0 +1,9 @@
+public class BrailleTranslation {
+
+    public static class Solution {
+        public static String solution(String s) {
+            return ""; //TODO
+        }
+    }
+    
+}
---
+++ src/test/java/BrailleTranslationTests.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class BrailleTranslationTests {
+
+    @Test
+    public void testWord() {
+        String s = BrailleTranslation.Solution.solution("code");
+        assertEquals(s, "100100101010100110100010");
+    }
+
+    @Test
+    public void testWordWithCapital() {
+        String s = BrailleTranslation.Solution.solution("Braille");
+        assertEquals(s, "000001110000111010100000010100111000111000100010");
+    }
+
+    @Test
+    public void testSentence() {
+        String s = BrailleTranslation.Solution.solution("The quick brown fox jumps over the lazy dog");
+        assertEquals(s, "000001011110110010100010000000111110101001010100100100101000000000110000111010101010010111101110000000110100101010101101000000010110101001101100111100011100000000101010111001100010111010000000011110110010100010000000111000100000101011101111000000100110101010110110");
+    }
+    
+}
