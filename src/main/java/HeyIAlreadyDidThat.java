c94f5e91319b62e0ac62524312266c0c4ec702c7
Rahul Yarlagadda <knufire@gmail.com>
2020-05-28 23:52:01 -0400

    Added Hey I Already Did That problem.

---
+++ src/main/java/HeyIAlreadyDidThat.java
@@ -0,0 +1,12 @@
+public class HeyIAlreadyDidThat {
+
+    public static class Solution {
+
+        public static int solution(String n, int b) {
+            // Your code here
+            return 0;
+        }
+
+    }
+    
+}
