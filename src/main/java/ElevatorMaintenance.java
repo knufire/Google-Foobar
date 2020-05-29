c8b372ae7d504e2939631081ecebfbd6b73c939b
Rahul Yarlagadda <knufire@gmail.com>
2020-05-28 22:16:13 -0400

    Added Elevator Maintenence problem.

---
+++ src/main/java/ElevatorMaintenance.java
@@ -0,0 +1,11 @@
+public class ElevatorMaintenance {
+
+    public static class Solution {
+
+        public static String[] solution(String[] l) {
+            String[] answer = {};
+            return answer;
+        }
+    }
+
+}
