a0bd0dc97b673d5a030e6e62c2107cbd6b57fd0e
Rahul Yarlagadda <knufire@gmail.com>
2020-05-31 19:40:26 -0400

    Find The Acess Code passes provided unit tests.

--- src/main/java/FindTheAccessCodes.java
+++ src/main/java/FindTheAccessCodes.java
@@ -1,9 +1,42 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class FindTheAccessCodes {
 
     public static class Solution {
 
         public static int solution(int[] l) {
-            return 0;
+
+            // Generate a DAG where an edge from u to v implies that v is divisible by u
+            Map<Integer, Set<Integer>> graph = new HashMap<>();
+            for (int i = 0; i < l.length; i++) {
+                Set<Integer> edges = new HashSet<>();
+                int u = l[i];
+                for (int j = i + 1; j < l.length; j++) {
+                    int v = l[j];
+                    if (v % u == 0) {
+                        // Nodes will be identified by index, not value, as values
+                        // may not be unique
+                        edges.add(j);
+                    }
+                }
+                graph.put(i, edges);
+            }
+
+            int result = 0;
+
+            for (int u : graph.keySet()) {
+                for (int v : graph.get(u)) {
+                    // There exists a edge from u to v. If v has an edge to any other
+                    // node, this forms a "lucky triplet"
+                    result += graph.get(v).size();
+                }
+            }
+
+            return result;
+
         }
     }
 
