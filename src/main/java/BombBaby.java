43f258bda3070f2d393ccb1116c8a73593d21721
Rahul Yarlagadda <knufire@gmail.com>
2020-06-01 03:35:53 -0400

    Added the Bomb, Baby! problem

---
+++ src/main/java/BombBaby.java
@@ -0,0 +1,10 @@
+public class BombBaby {
+    
+    public static class Solution {
+
+        public static String solution(String x, String y) {
+            return "";
+        }
+        
+    }
+}
